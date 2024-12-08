6355c7e6ad993c23a91eb4cbca77d32cf0ead9ff
Syed Mehdi <mehdisyed3@gmail.com>
2024-12-08 16:06:56 -0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import {
 } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
-// import SinglePage from "./routes/singlePage/singlePage";
-// import ProfilePage from "./routes/profilePage/profilePage";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 
@@ -26,14 +26,14 @@ function App() {
           path:"/list",
           element:<ListPage/>
         },
-        // {
-        //   path:"/:id",
-        //   element:<SinglePage/>
-        // },
-        // {
-        //   path:"/profile",
-        //   element:<ProfilePage/>
-        // },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
         {
           path:"/login",
           element:<Login/>
